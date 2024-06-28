eabda3bffae93559a81c7f451c19c415de358af7
marcosd59 <damian.marcospool@gmail.com>
2024-06-28 08:07:43 -0500

    chore: Refactor queryEmbeddings and queryLLM functions

--- frontend/src/routes/bot/+page.svelte
+++ frontend/src/routes/bot/+page.svelte
@@ -12,7 +12,6 @@
 		documents: string[][];
 		distances: number[][];
 		metadatas: MetaData[][];
-		arsedTextBlocks: TextBlock[][];
 	}
 
 	interface MetaData {
@@ -20,67 +19,33 @@
 		file_name: string;
 	}
 
-async function queryEmbeddings() {
-    loading = true;
-    try {
-        const response = await fetch(`${PUBLIC_PROCESS_FILES_SERVER}/query?text=${query}`, {
-            method: 'GET'
-        });
-        console.log('response: ', parsedTextBlocks);
-        if (response.ok) {
-            let result: QueryResult = await response.json();
-            if (result.documents.length > 0) {
-                documents = result.documents[0];
-                metadatas = result.metadatas[0];
-                parsedTextBlocks = await queryLLM();
-            } else {
-                console.error('No documents found.');
-                documents = [];
-                metadatas = [];
-                parsedTextBlocks = [];
-            }
-        } else {
-            console.error('Failed to fetch data');
-            parsedTextBlocks = [];
-        }
-    } catch (error) {
-        console.error('Error querying embeddings:', error);
-        parsedTextBlocks = [];
-    } finally {
-        loading = false;
-    }
-}
+	async function queryEmbeddings() {
+		loading = true;
+		const response = await fetch(`${PUBLIC_PROCESS_FILES_SERVER}/query?text=${query}`, {
+			method: 'GET'
+		});
+		console.log('test');
+		let result: QueryResult = await response.json();
+		documents = result.documents[0];
+		metadatas = result.metadatas[0];
+		await queryLLM();
+	}
 
-async function queryLLM(): Promise<TextBlock[]> {
-    try {
-        const response = await fetch('/query_bot', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                query: query,
-                documents: documents
-            })
-        });
-				console.log('response: ', response);
-        if (response.ok) {
-            let result = await response.json();
-						console.log('result: ', result);
-						console.log('result.answer: ', result.answer);
-						console.log('parseText(result.answer): ', parseText(result.answer));
-						console.log("Success query LLM")
-            return parseText(result.answer);
-        } else {
-            console.error('Failed to query LLM');
-            return [];
-        }
-    } catch (error) {
-        console.error('Error querying LLM:', error);
-        return [];
-    }
-}
-
+	async function queryLLM() {
+		const response = await fetch('/query_bot', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				query: query,
+				documents: documents
+			})
+		});
+		let result = await response.json();
+		parsedTextBlocks = parseText(result.answer);
+		loading = false;
+	}
 
 	interface TextBlock {
 		isCodeBlock: boolean;
@@ -115,9 +80,8 @@ async function queryLLM(): Promise<TextBlock[]> {
 
 <div class="flex justify-center items-center p-4">
 	<div class="m-11 card h-full w-3/4">
-		<div class="p-4 md:p-10 tarjeta">
-			<div class="centro">
-				<h1>🤖 Bot Unicaribe</h1>
+		<div class="p-4 md:p-10">
+			<h1>🤖 Bot Unicaribe</h1>
 				<br />
 				<h3 class="mb-3">El asesor de la Universidad del Caribe!</h3>
 			</div>
@@ -126,25 +90,25 @@ async function queryLLM(): Promise<TextBlock[]> {
 				<input
 					class="input text-xl"
 					type="text"
-					placeholder="Ej. ¿Cuál es el horario de la universidad?"
+					placeholder="your question here"
 					bind:value={query}
 				/>
 				<button
 					type="button"
-					class="btn variant-filled-secondary w-1/5 ml-4 text-xl btn-responsive"
-					on:click={queryEmbeddings}>Preguntar</button
+					class="btn variant-filled-secondary w-1/5 ml-4 text-xl"
+					on:click={queryEmbeddings}>Preguntar: </button
 				>
 			</div>
 
 			{#if loading}
-				<div class="p-8" style="color: white;">
-					Pensando...<br />
+				<div class="p-8">
+					Searching...<br />
 					<ProgressBar height="h-3" meter="bg-warning-500" />
 				</div>
 			{:else}
 				{#if parsedTextBlocks.length > 0}
 					<div>
-						<h3 class="text-xxl mb-2 mt-6">Respuesta</h3>
+						<h3 class="text-xxl mb-2 mt-6">Respuesta: </h3>
 						<div class="flex flex-col rounded-[16px] bg-tertiary-500 p-4">
 							{#each parsedTextBlocks as textBlock}
 								{#if textBlock.isCodeBlock}
@@ -162,7 +126,7 @@ async function queryLLM(): Promise<TextBlock[]> {
 						{#each documents as document, i}
 							<div class="flex flex-col">
 								<CodeBlock language="markdown" code={document} />
-								<p class="mt-2">Archivo Fuente: {metadatas[i].file_name}</p>
+								<p class="mt-2">Archivo: {metadatas[i].file_name}</p>
 							</div>
 							<br />
 						{/each}
@@ -172,26 +136,3 @@ async function queryLLM(): Promise<TextBlock[]> {
 		</div>
 	</div>
 </div>
-
-<style>
-	.tarjeta {
-		background-color: #212121;
-	}
-
-	h1,
-	h3,
-	p {
-		color: white;
-	}
-
-	.centro {
-		text-align: center;
-	}
-
-	@media (max-width: 600px) {
-		.btn-responsive {
-			font-size: 0.575rem;
-			padding: 0.5rem 1rem;
-		}
-	}
-</style>
